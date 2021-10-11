8058472e459bb48a85585ef8ed5e38f9b9955b7f
Allan Pennycuff <allanp1238@gmail.com>
2021-10-11 14:25:45 -0500

    Completed script.js file for Loops Exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+for (i = 1; i <= 7; i++){
+  console.log(i);
+};
+for (j = 5; j <= 25; j += 4){
+  console.log(j);
+};
+const wizards = [
+  "Harry Potter",
+  "Hermione Granger",
+  "Ron Weasley"
+];
+for (char of wizards){
+  console.log(char);
+};
+let harryPotterMovies = 0;
+while (harryPotterMovies < 8){
+  harryPotterMovies++
+}
+console.log(harryPotterMovies);
